5c88a7ecbda0f25f4d04360b5b84eb67aac90337
Lenny Sirivong <lenny@lenny-s.com>
2018-12-02 10:04:36 -0700

    Prison generates hostages

--- Assets/Prison.cs
+++ Assets/Prison.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Prison : MonoBehaviour {
+  [SerializeField]
+  int numOccumpants = 0;
+
+  [SerializeField]
+  GameObject occupantPrefab;
 
 	// Use this for initialization
 	void Start () {
-		
+    for (int i = 0; i < numOccumpants; i++) {
+      GameObject o = Instantiate(
+        occupantPrefab,
+        // could be an issue if too many objects
+        transform.position + transform.forward * i * 1f,
+        Quaternion.identity,
+        transform
+      );
+
+      print("HOSTAGE: " + o.transform.position);
+    }
 	}
 	
 	// Update is called once per frame
